2ad22c47841a2e41aa6d55803656d66200bea920
leeeeegun <leeeeegun@naver.com>
2025-08-21 09:43:31 +0900

    feat: [이의건] entity 기본 구성 [AVB-BACK-01]

--- src/main/java/com/codingrecipe/anonymousvotebackend/entity/Question.java
+++ src/main/java/com/codingrecipe/anonymousvotebackend/entity/Question.java
@@ -1,11 +1,11 @@
 package com.codingrecipe.anonymousvotebackend.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,5 +19,9 @@ public class Question {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String text;
 
+    @ElementCollection
+    private List<String> options = new ArrayList<>();
+    // 선택지 (예: "가", "나", "다")
 }
